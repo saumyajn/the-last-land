501c78bce71bcefdf23e1ac94dd430602e0d97be
Saumya Jain <saums06@gmail.com>
2025-04-25 00:47:46 -0400

    updated admin users

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,5 +4,8 @@ export const ADMIN_EMAILS = [
     "sanketvazesvsvsv@gmail.com",
     "saumyajn1994@gmail.com",
     "evil.micha.777@gmail.com",
-    "selistongama194@gmail.com"
+    "selistongama194@gmail.com",
+    "silenttkkiller2@gmail.com",
+     "angelaquino621@gmail.com",
+    "coemaincastle@gmail.com"
 ]
